9d44073f92ca7cea915f805bcdd9e52b675bdb5f
Daniel Baker <daniel.n.baker@gmail.com>
2023-11-10 23:42:12 -0800

    Undo automatically running ssh-add. It caused the ssh-agent to fail.

--- hm/services.nix
+++ hm/services.nix
@@ -1,4 +1,7 @@
 {pkgs, ...}: {
   services.ssh-agent.enable = true;
-  systemd.user.services.ssh-agent.Service.ExecStartPost = "${pkgs.openssh}/bin/ssh-add ~/.ssh/github";
+
+  # This didn't work.
+  # Nov 10 22:59:35 iglooo ssh-add[1323]: Could not open a connection to your authentication agent.
+  # systemd.user.services.ssh-agent.Service.ExecStartPost = "${pkgs.openssh}/bin/ssh-add home/bakerdn/.ssh/github";
 }
