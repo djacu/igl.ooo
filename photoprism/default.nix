c4dcbcdbdb4cfbc02412762b673830848177a564
Daniel Baker <daniel.n.baker@gmail.com>
2023-11-19 15:47:12 -0800

    Removed recommended settings from nixos wiki that didn't work.

--- photoprism/default.nix
+++ photoprism/default.nix
@@ -50,22 +50,13 @@ in {
 
   # nginx
   services.nginx = {
-    #recommendedTlsSettings = true;
-    #recommendedOptimisation = true;
-    #recommendedGzipSettings = true;
-    #recommendedProxySettings = true;
-    #clientMaxBodySize = "500m";
     virtualHosts.${fqdn} = {
       enableACME = true;
       forceSSL = true;
-      # http2 = true;
       locations = {
         "/" = {
           recommendedProxySettings = true;
           proxyPass = "http://127.0.0.1:${builtins.toString cfg.port}/";
-          # proxyWebsockets = true;
-          #  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          #  proxy_set_header Host $host;
 
           extraConfig = ''
             proxy_buffering off;
